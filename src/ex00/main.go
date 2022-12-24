077c31e59327b7d579fc690e7bd66a15ea3b38c0
Madcow1313 <safbul@gmail.com>
2022-12-24 12:48:51 +0300

    i have no idea what changed

--- src/ex00/main.go
+++ src/ex00/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 type reqBody struct {
@@ -19,13 +20,15 @@ type response struct {
 
 func simpleHandler(writer http.ResponseWriter, request *http.Request) {
 	if request.URL.Path != "/buy_candy" {
+		writer.Header().Set("Content-Type", "application/json")
 		http.Error(writer, "404 not found", http.StatusNotFound)
 		return
 	}
 	var requestBody reqBody
 	err := json.NewDecoder(request.Body).Decode(&requestBody)
 	if err != nil {
-		http.Error(writer, err.Error(), http.StatusBadRequest)
+		writer.Header().Set("Content-Type", "application/json")
+		http.Error(writer, "400 Bad request", http.StatusBadRequest)
 		return
 	}
 	candyMap := map[string]int{
@@ -38,19 +41,21 @@ func simpleHandler(writer http.ResponseWriter, request *http.Request) {
 	switch request.Method {
 	case "POST":
 		if err := request.ParseForm(); err != nil {
+			writer.Header().Set("Content-Type", "application/json")
 			http.Error(writer, err.Error(), http.StatusBadRequest)
 			return
 		}
 		if value, key := candyMap[requestBody.CandyType]; key && requestBody.Money > 0 && requestBody.CandyCount > 0 {
 			if requestBody.Money >= value*requestBody.CandyCount {
-				writer.WriteHeader(http.StatusCreated)
-				writer.Header().Set("Content-Type", "application/json")
 				resp := response{requestBody.Money - value*requestBody.CandyCount, "Thank you"}
 				jsonResp, err := json.Marshal(resp)
 				if err != nil {
+					writer.WriteHeader(http.StatusBadRequest)
 					http.Error(writer, "400 Bad request", http.StatusBadRequest)
 					return
 				}
+				writer.Header().Set("Content-Type", "application/json")
+				writer.WriteHeader(http.StatusCreated)
 				writer.Write(jsonResp)
 			} else {
 				writer.WriteHeader(http.StatusPaymentRequired)
@@ -58,15 +63,21 @@ func simpleHandler(writer http.ResponseWriter, request *http.Request) {
 				fmt.Fprintf(writer, "You need {%d} more money!\n", candyMap[requestBody.CandyType]-requestBody.Money)
 			}
 		} else {
+			writer.Header().Set("Content-Type", "application/json")
 			http.Error(writer, "400 Bad request", http.StatusBadRequest)
 		}
 
 	default:
+		writer.Header().Set("Content-Type", "application/json")
 		http.Error(writer, "405 Method not allowed", http.StatusMethodNotAllowed)
 	}
 }
 
 func main() {
 	http.HandleFunc("/", simpleHandler)
-	http.ListenAndServe(":3333", nil)
+	err := http.ListenAndServe(":3333", nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
 }
